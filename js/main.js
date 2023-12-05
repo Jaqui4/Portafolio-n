aba134bbbbac7d3e998a3f2c4724bc6568241f48
jaqui <jaquelin.mfarango@gmail.com>
2023-12-05 00:50:05 -0600

    Se agregan animaciones js en el inicio, se implementa hoover en el menú, se cambia foto principal, se agrega link de cv.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Animaciones en Inicio
+const typed = new Typed ('.inicio3', {
+    strings: ['Desarrolladora Web Jr.','Psicóloga ♡'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true,
+});
+
+const typed2 = new Typed ('.inicio1', {
+    strings: ["It's me ♪","Hi! ","I'm..."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+});
